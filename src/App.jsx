5b550244063cd80886055d7176394d9926d16360
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-11-29 11:32:15 +0530

    updated .eslintrc.cjs

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-        <h1 className='px-4 py-4 mt-5'>Hi there</h1>
-    </div>
+    <main className='relative'>
+      <Nav>
+        <h1>Hi</h1>
+      </Nav>
+    </main>
   )
 }
 
